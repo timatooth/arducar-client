f753bfb6b55edcb3ca4c588ddbd86d574f6ae6c8
Tim Sullivan <tsullivan@timatooth.com>
2014-10-10 13:16:11 +1300

    started client

---
+++ src/main/java/com/timatooth/carclient/CarConnection.java
@@ -0,0 +1,48 @@
+package com.timatooth.carclient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tim
+ */
+public class CarConnection {
+
+    private final String host;
+    private final Integer port;
+    private Socket socket;
+    private BufferedOutputStream out;
+    private BufferedInputStream in;
+
+    public CarConnection(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void connect() throws IOException {
+        socket = new Socket(host, port);
+        out = new BufferedOutputStream(socket.getOutputStream());
+        in = new BufferedInputStream(socket.getInputStream());
+
+    }
+
+    public void sendBytes(byte[] b) throws IOException {
+        System.out.println("Sending: " + b[0]);
+        out.write(b[0]);
+        out.flush();
+    }
+    
+    public void disconnect(){
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(CarConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
